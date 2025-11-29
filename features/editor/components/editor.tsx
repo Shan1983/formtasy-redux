39a3ba40762839aca004a81b40e35a6c0bd4441c
Shannan Galbraith <s.galbraith08@gmail.com>
2025-11-30 00:53:18 +1100

    added the base editor

---
+++ features/editor/components/editor.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  type Node,
+  type Edge,
+  type NodeChange,
+  type EdgeChange,
+  type Connection,
+  Background,
+  MiniMap,
+  Panel,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { nodeComponents } from "@/config/node-components";
+import { FlowControls } from "./flow-controls";
+import { ErrorView, LoadingView } from "@/components/core-components";
+import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+
+export const EditorLoading = () => {
+  return <LoadingView message="Loading editor..." />;
+};
+
+export const EditorError = () => {
+  return <ErrorView message="Error loading editor" />;
+};
+
+export const Editor = ({ workflowId }: { workflowId: string }) => {
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+
+  const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+  const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+  const [showMiniMap, setShowMiniMap] = useState(false);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    []
+  );
+  const onConnect = useCallback(
+    (params: Connection) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    []
+  );
+  const toggleMiniMap = useCallback(
+    () => setShowMiniMap((current) => !current),
+    []
+  );
+
+  return (
+    <div className="size-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        proOptions={{ hideAttribution: true }}
+        nodeTypes={nodeComponents}
+      >
+        {showMiniMap ? <MiniMap /> : null}
+        <Panel position="bottom-center">
+          <FlowControls
+            showMiniMap={showMiniMap}
+            onToggleMiniMap={toggleMiniMap}
+          />
+        </Panel>
+      </ReactFlow>
+    </div>
+  );
+};
